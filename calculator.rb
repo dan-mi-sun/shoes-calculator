2b6ee33dceebab768c2304bf8ee46f5556ee74d2
Daniel Sun <dan.mi.sun@gmail.com>
2014-06-11 17:49:58 +0100

    refactor functions to use a case statement

--- calculator.rb
+++ calculator.rb
@@ -6,21 +6,20 @@ Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
     flow do
       %w(7 8 9 4 5 6 1 2 3 0 + / * -).each do |op|      
         button op do         
-          append op
+          case op
+          when ('0'..'9')
+            append(op)
+          when "="
+            eval_expression
+          when "C"
+            clear_output
+          end
         end
       end
-
-      button "C" do
-        clear_output
-      end
-      
-      button "=" do
-        eval_expression
-      end
     end
-    
+
   end
-  
+
   # Stick a string on the end of our input
   #
   def append(s)
@@ -30,7 +29,7 @@ Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
     @input += s
     @output.text = @input
   end
-  
+
   # Evaluate the input we've got so far
   #
   def eval_expression
@@ -42,5 +41,5 @@ Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
     @output.text = ""
     @input = nil
   end
-  
+
 end
